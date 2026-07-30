2be7124423f1501224cd23567b6992de4ff76fc1
Ithesh K <ithesh9786@gmail.com>
2026-07-30 16:42:50 +0530

    Add slideshow functionality to script.js

--- script.js
+++ script.js
@@ -44,3 +44,23 @@ document.getElementById("admissionForm").addEventListener("submit", function(e) 
         console.log(error);
     });
 });
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 3000);
+}
